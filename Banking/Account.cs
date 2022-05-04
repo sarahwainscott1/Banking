439c8ffd3905c4c462b67d5ac5eaca3148661eb5
Sarah Wainscott <swainscott@tql.com>
2022-05-04 16:17:38 -0400

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+    internal class Account {
+        private static int NextId = 1;
+        public int AcctNbr { get; private set; } = 0; //private set says can only change within account class, not in program
+        public string Name { get; set; } = string.Empty;
+        public decimal Balance { get; private set; } = 0;
+
+        public bool Deposit(decimal Amount) {
+            if (Amount < 0) {
+                Console.WriteLine("Amount must be greater than 0.");
+                return false;
+                }
+            Balance = Balance + Amount;
+            return true; 
+            }
+        public bool Withdraw(decimal Amount) {
+                if (Amount < 0) {
+                    Console.WriteLine("Amount must be greater than 0.");
+                    return false;
+                    }
+                if (Amount > Balance) { //error statement
+                        Console.WriteLine("Insufficient Balance :(");
+                        return false;
+                     }
+                Balance = Balance - Amount;
+                return true;
+                }
+        public bool Transfer(decimal Amount, Account DAccount) { //assume withdraw from this acct, must set deposit account
+            if(Withdraw(Amount)) { //already returns a boolean so no need to type it as a boolean here
+                DAccount.Deposit(Amount); //sets deposits to go to daccount
+                return true;
+                }
+            return false;
+            }
+
+        public Account() {
+            AcctNbr = NextId++;
+            }
+        }
+    }
---
+++ Banking/Program.cs
@@ -0,0 +1,27 @@
+﻿using Banking;
+
+var acct1 = new Account() {
+    Name = "Checking1"
+    };
+var acct2 = new Account() {
+    Name = "Checking2"
+    };
+
+acct1.Deposit(1000);
+acct2.Deposit(500);
+
+acct1.Withdraw(200);
+acct2.Withdraw(50);
+
+Console.WriteLine($"{acct1.Name} Balance is {acct1.Balance:c}.\n{acct2.Name} Balance is {acct2.Balance:c}."); //:c formats for currency
+
+acct1.Withdraw(1000); // should fail because after above bal is 800
+
+Console.WriteLine($"{acct1.Name} Balance is {acct1.Balance:c}.\n{acct2.Name} Balance is {acct2.Balance:c}.");
+
+acct2.Transfer(100, acct1);
+Console.WriteLine($"{acct1.Name} Balance is {acct1.Balance:c}.\n{acct2.Name} Balance is {acct2.Balance:c}.");
+
+acct1.Deposit(-100); //deposit negative number acts like a withdraw. negative withdraw would act like deposit
+acct2.Withdraw(-200);
+Console.WriteLine($"{acct1.Name} Balance is {acct1.Balance:c}.\n{acct2.Name} Balance is {acct2.Balance:c}.");
